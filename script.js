8853844058821acb364ea652f6416635046792fb
Alexis <alexis.pereira@estiam.com>
2024-07-15 12:09:59 +0200

    3e commit

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ AOS.init({
     duration: 1000,
     once: true
 });
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav .menu ul li a');
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        const sectionId = section.getAttribute('id');
+
+        if (pageYOffset >= sectionTop - sectionHeight / 2) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${sectionId}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+});
